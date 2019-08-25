be3a32e0b8dde9834b31bce41f2dd1a25017e951
Amer <amer.kawar@tweepi.com>
2019-08-25 15:02:40 -0700

    v0.1.2, save meta not working? added debugging message

--- src/water_dose.py
+++ src/water_dose.py
@@ -17,7 +17,7 @@ class WaterDose():
         # TODO: Better source per plant
         'plant_adult_age_weeks': 15,
         # Magic mutiplier to convert plant size to ml needed for watering
-        'to_ml_multiplier': 2
+        'to_ml_multiplier': 3
     }
 
     def __init__(self, farmwarename, config):
@@ -40,28 +40,29 @@ class WaterDose():
             return False
 
         # get OpenFarm data
+        log("Plant meta: {}".format(plant['meta']), "success", title="calc_watering_params")
+
         if 'height' not in plant['meta'] or 'spread' not in plant['meta']:
             try:
                 res = Openfarm.get_openfarm_attribs(plant['openfarm_slug'])
 
-                s = plant['meta']['spread'] = res['spread'] * 10
-                h = plant['meta']['height'] = res['height'] * 10
+                plant['meta']['spread'] = res['spread'] * 10
+                plant['meta']['height'] = res['height'] * 10
 
                 # save the spread, height data
                 Plants.save_plant({
                     'id': plant['id'],
                     'meta': {
-                        'spread': s,
-                        'height': h
+                        'spread': plant['meta']['spread'],
+                        'height': plant['meta']['height']
                     }
                 })
-
             except Exception as e:
                 log("Openfarm did not return results for {}, consider updated OpenFarm entry. {}".format(
                     plant['openfarm_slug'], e),
                     title="calc_watering_params")
-                plant['meta']['spread'] = 5
-                plant['meta']['height'] = 5
+                plant['meta']['spread'] = 5 * 10
+                plant['meta']['height'] = 5 * 10
         else:
             plant['meta']['spread'] = int(plant['meta']['spread'])
             plant['meta']['height'] = int(plant['meta']['height'])
@@ -69,9 +70,9 @@ class WaterDose():
         # calculating supposed watering for today
         supposed_watering = self._get_supposed_watering(plant['meta']['spread'], age)
 
-        # how much to water in ml
+        # how much to water in ml, minimum value for ms is 250ms
         ml = int(round(supposed_watering)) if supposed_watering > 0 else 0
-        ms = int(ml / self.config["water_ml_per_sec"] * 1000)
+        ms = min(int(ml / self.config["water_ml_per_sec"] * 1000), 250)
 
         log("Water {} {}ml; for {}ms".format(plant['openfarm_slug'], ml, ms),
             title="calc_watering_params")
@@ -87,12 +88,7 @@ class WaterDose():
 
     # return supposed watering for the plant with the given spread and age
     def _get_supposed_watering(self, max_spread, age):
-        min_d = 2
-
         step = max_spread / float(self.config["plant_adult_age_weeks"] * 7)
         d = step * age
-
-        if d < min_d:
-            d = min_d
 
         return int(d * self.config["to_ml_multiplier"])
